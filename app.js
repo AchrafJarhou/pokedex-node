4615cf7b5685b062d17456caf086757df9f0130d
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2025-10-21 16:21:41 +0200

    creation endpoint static pour recuperer un pokemon

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello again World !");
+});
+app.get("/api/pokemons/1", (req, res) => {
+  res.send("hello pikachu");
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
