fd7976bd9d37b383dfdfb62ad3e04cb76f635de2
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2025-10-21 16:28:17 +0200

    rendre endpoint pour recuperer un pokemon dynamique

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("Hello again World !");
 });
-app.get("/api/pokemons/1", (req, res) => {
-  res.send("hello pikachu");
+app.get("/api/pokemons/:id", (req, res) => {
+  const id = req.params.id;
+  res.send("You requested the pokemon with id: " + id);
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
