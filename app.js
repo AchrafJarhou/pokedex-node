99df27a7e7b8cc38ae362a20975aba13f5daf4d6
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2025-10-21 16:50:57 +0200

    ajout fichier mock-pokemon qui a tous les pokemon et affichage du nom du pokemon dans le endpoint qui recupere un pokemon

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require("express");
+let pokemons = require("./mock-pokemon");
+
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
   res.send("Hello again World !");
 });
 app.get("/api/pokemons/:id", (req, res) => {
-  const id = req.params.id;
-  res.send("You requested the pokemon with id: " + id);
+  const id = parseInt(req.params.id);
+  const pokemon = pokemons.find((pokemon) => pokemon.id === id);
+  res.send(
+    "You requested the pokemon with id: " + id + " which is " + pokemon.name
+  );
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
