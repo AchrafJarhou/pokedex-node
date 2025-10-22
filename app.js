02ba4859856df67bd6aa029e76ca9060e8be5522
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2025-10-22 11:58:39 +0200

    recuperation la function succes dans app grace a laffectation destrecturé et pas tou le helpers

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const helper = require("./helpers");
+const { success } = require("./helpers");
 let pokemons = require("./mock-pokemon");
 
 const app = express();
@@ -10,13 +10,13 @@ app.get("/", (req, res) => {
 app.get("/api/pokemons", (req, res) => {
   const count = pokemons.length;
   const message = "La liste des pokémons a été récupérée avec succès.";
-  res.json(helper.success(message, pokemons));
+  res.json(success(message, pokemons));
 });
 app.get("/api/pokemons/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const message = "Un pokémon a été trouvé.";
   const pokemon = pokemons.find((pokemon) => pokemon.id === id);
-  res.json(helper.success(message, pokemon));
+  res.json(success(message, pokemon));
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
