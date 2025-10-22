791ac3b61c0d930732cbcfce6778f3d978e5f049
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2025-10-22 13:42:38 +0200

    ajout dun middlewares et recuperation url el ma thode de la requette

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ let pokemons = require("./mock-pokemon");
 
 const app = express();
 const port = 3000;
+const logger = (req, res, next) => {
+  console.log(`Request received: ${req.method} ${req.url}`);
+  next();
+};
+app.use(logger);
 app.get("/", (req, res) => {
   res.send("Hello again World !");
 });
