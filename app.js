b022968dcb4a7c7645e7a778e6bee4f799a0ae71
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2025-10-23 19:02:20 +0200

    ajout endpoint qui permet de suprimer un pokemon

--- app.js
+++ app.js
@@ -44,6 +44,13 @@ app.put("/api/pokemons/:id", (req, res) => {
   const message = `Le pokémon ${pokemonUpdated.name} a bien été modifié.`;
   res.json(success(message, pokemonUpdated));
 });
+app.delete("/api/pokemons/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const pokemonDeleted = pokemons.find((pokemon) => pokemon.id === id);
+  pokemons = pokemons.filter((pokemon) => pokemon.id !== id);
+  const message = `Le pokémon ${pokemonDeleted.name} a bien été supprimé.`;
+  res.json(success(message, pokemonDeleted));
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
