d5ae815cc9cd597d25eac5062161d9428af97835
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2025-10-24 14:39:11 +0200

    ajout dun geteur et seteur pour le types dun pokemon

--- src/models/pokemon.js
+++ src/models/pokemon.js
@@ -1,34 +1,44 @@
 /* L’API Rest et la Base de données : Créer un modèle Sequelize */
 module.exports = (sequelize, DataTypes) => {
-  return sequelize.define('Pokemon', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
+  return sequelize.define(
+    "Pokemon",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      hp: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      cp: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      picture: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      types: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        get() {
+          return this.getDataValue("types").split(",");
+        },
+        set(types) {
+          this.setDataValue("types", types.join());
+        },
+      },
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    hp: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    cp: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    picture: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    types: {
-      type: DataTypes.STRING,
-      allowNull: false
+    {
+      timestamps: true,
+      createdAt: "created",
+      updatedAt: false,
     }
-  }, {
-    timestamps: true,
-    createdAt: 'created',
-    updatedAt: false
-  })
-}
+  );
+};
