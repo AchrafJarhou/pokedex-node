14b7ef569a7c2a71e7e785eada68e6fc405ddb8c
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2025-10-28 14:26:44 +0100

    gestion derreur pour authentifier un utlisateur

--- src/routes/login.js
+++ src/routes/login.js
@@ -3,15 +3,28 @@ const bcrypt = require("bcrypt");
 
 module.exports = (app) => {
   app.post("/api/login", (req, res) => {
-    User.findOne({ where: { username: req.body.username } }).then((user) => {
-      bcrypt
-        .compare(req.body.password, user.password)
-        .then((isPasswordValid) => {
-          if (isPasswordValid) {
+    User.findOne({ where: { username: req.body.username } })
+      .then((user) => {
+        if (!user) {
+          const message =
+            "L'utilisateur demandé n'existe pas. Vérifiez le nom d'utilisateur.";
+          return res.status(404).json({ message });
+        }
+        bcrypt
+          .compare(req.body.password, user.password)
+          .then((isPasswordValid) => {
+            if (!isPasswordValid) {
+              const message = `le mot de passe est incorrect. Veuillez réessayer.`;
+              return res.status(401).json({ message });
+            }
             const message = `L'utilisateur a été connecté avec succès`;
             return res.json({ message, data: user });
-          }
-        });
-    });
+          });
+      })
+      .catch((error) => {
+        const message =
+          "Une erreur est survenue lors de la tentative de connexion.";
+        res.status(500).json({ message, data: error });
+      });
   });
 };
